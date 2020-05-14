b7d7046e5b9167c2889332e536bec32966e6e7b2
tensice <tensice@gmail.com>
2020-05-14 22:13:04 +0000

    right left

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-print(87 * 34)
-# This is a comment!!!!
-print(120)
-print('45 * 63 = ' + str(45 * 63))
-print(15/3)
-print(5 + 3 + 1 * 9 / 5 + 4 - 8) 
-print(4 + 2 * 8)
-print('Sun' + 'flower')
-print('''eat
-food''')
+print('right' + "left")
