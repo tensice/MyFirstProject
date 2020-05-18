58578b2aec9a780b5f04fb31ebd5da1c05913b49
tensice <tensice@gmail.com>
2020-05-18 18:28:54 +0000

    good code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('right' + "left")
+
+
+"""
+print(87 * 34)
+# This is a comment!!!!
+print(120)
+print('45 * 63 = ' + str(45 * 63))
+print(15/3)
+print(5 + 3 + 1 * 9 / 5 + 4 - 8) 
+print(4 + 2 * 8)
+print('Sun' + 'flower')
+print('''eat
+food''')
+"""
+first_name = input('Type your first name\n')
+last_name = input('Type your last name\n')
+address = input('Type your address\n')
+zipcode = input('Type your zipcode\n')
+birth_year = input('Type your birth year\n')
+age = 2020 - int(birth_year)
+print(f'Name: {first_name}  {last_name}')
+
+print(f'Address:{address}')
+print(f'Zipcode:{zipcode}')
+print(f'Age: {age}')
+
