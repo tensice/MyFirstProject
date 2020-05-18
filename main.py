2272485cf1dbde4d82eda475a75a839758ca3b70
tensice <tensice@gmail.com>
2020-05-18 20:34:42 +0000

    temperature

--- main.py
+++ main.py
@@ -30,8 +30,13 @@ b = 3
 print(a / b)
 print(a // b)
 """
+"""
 minutes = input('Enter minutes\n')
 hours = int(minutes) // 60
 mins = int(minutes) % 60
 print(f'Hours:{hours}')
 print(f'Minutes:{mins}')
+"""
+farenheit = int(input('Enter temperature in farenheit\n'))
+celsius = (farenheit - 32) * 5/9
+print(f'x = {farenheit}  y = {celsius}')
