91e7f3fb8b3a103ca0161eedd1a5d3cb16bffeab
tensice <tensice@gmail.com>
2020-05-18 19:14:56 +0000

    hour minute converter

--- main.py
+++ main.py
@@ -12,15 +12,26 @@ print('Sun' + 'flower')
 print('''eat
 food''')
 """
+"""
 first_name = input('Type your first name\n')
 last_name = input('Type your last name\n')
 address = input('Type your address\n')
 zipcode = input('Type your zipcode\n')
 birth_year = input('Type your birth year\n')
 age = 2020 - int(birth_year)
-print(f'Name: {first_name}  {last_name}')
+print(f'Name: {first_name} {last_name}')
 
 print(f'Address:{address}')
 print(f'Zipcode:{zipcode}')
 print(f'Age: {age}')
-
+"""
+"""a = 20 
+b = 3
+print(a / b)
+print(a // b)
+"""
+minutes = input('Enter minutes\n')
+hours = int(minutes) // 60
+mins = int(minutes) % 60
+print(f'Hours:{hours}')
+print(f'Minutes:{mins}')
